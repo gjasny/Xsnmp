d497045968865545ec512e96154b130b47708bdb
Gregor Jasny <gjasny@googlemail.com>
2012-10-16 18:30:35 +0200

    Dropped PackageMaker packaging
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

--- packaging/scripts/prepare_postinstall.sh
+++
@@ -1,4 +0,0 @@
-echo "" >> /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall
-echo "# Return 0 to ensure installer is happy" >> /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall
-echo "exit 0" >> /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall
-echo "" >> /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall
