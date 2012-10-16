09591f2094ca6b13a29b8270d2ab4b3197dcd346
Gregor Jasny <gjasny@googlemail.com>
2012-10-16 18:23:43 +0200

    Drop old build system
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

--- build.sh
+++ build.sh
@@ -1,38 +1,13 @@
-BASEDIR=$PWD
+#!/bin/bash
 
-sudo rm -rf /Library/Xsnmp
+echo "Cleaning Xsnmp"
 
-cd deps
-tar zxvf pcre-8.02.tar.gz
-cd pcre-8.02
-$BASEDIR/util/pcre_build.sh
-cd $BASEDIR
+xcodebuild -project Xsnmp.xcodeproj clean
 
-/Xcode3/usr/bin/make distclean
-PATH="/Xcode3/usr/bin:/Xcode3/usr/sbin:$PATH" \
-./bootstrap_osx.sh && /Xcode3/usr/bin/make && sudo /Xcode3/usr/bin/make install
+echo "Building Xsnmp"
 
-sudo cp launchd/com.xsnmp.xsnmp-agentx.plist /Library/Xsnmp/XsnmpAgentExtension.app/Resources
-sudo cp -r packaging/XsnmpInstaller.pmdoc /Library/Xsnmp/XsnmpAgentExtension.app/Resources
-sudo cp -r packaging/Finished.rtfd /Library/Xsnmp/XsnmpAgentExtension.app/Resources
-sudo cp -r packaging/scripts /Library/Xsnmp/XsnmpAgentExtension.app/Resources
-sudo cp /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall.template /Library/Xsnmp/XsnmpAgentExtension.app/Resources/scripts/postinstall
-sudo ./packaging/scripts/prepare_postinstall.sh
-sudo cp packaging/Info.plist /Library/Xsnmp/XsnmpAgentExtension.app/Contents 
-sudo cp util/xsnmp_debug_info.sh /Library/Xsnmp/XsnmpAgentExtension.app/Contents/MacOS
-chnod ug+x /Library/Xsnmp/XsnmpAgentExtension.app/Contents/MacOS/xsnmp_debug_info.sh
+xcodebuild -project Xsnmp.xcodeproj -scheme Xsnmp build
 
-cd prefpane
-MVERS=`agvtool mvers | grep 'Found CFBundleShortVersionString' | sed 's/^[^\"]*\"//g' | sed 's/".*//g'`
-VERS=`agvtool vers | egrep '^[ ]*[0-9]+' | sed 's/^[ ]*//g'`
-PATH="/Xcode3/usr/bin:/Xcode3/usr/sbin:$PATH" \
-/Xcode3/usr/bin/xcodebuild -configuration Release clean
-PATH="/Xcode3/usr/bin:/Xcode3/usr/sbin:$PATH" \
-/Xcode3/usr/bin/xcodebuild -configuration Release build
-cd ..
+echo "Packaging Xsnmp"
 
-sudo cp -r prefpane/build/release/Xsnmp.prefPane /Library/Xsnmp/XsnmpAgentExtension.app/Resources
-
-/Xcode3/usr/bin/packagemaker --doc "/Library/Xsnmp/XsnmpAgentExtension.app/Resources/XsnmpInstaller.pmdoc" --out "packaging/Xsnmp-Installer-$MVERS.pkg" --title "Xsnmp $MVERS Installer"
-
-#sudo rm -rf /Library/Xsnmp
+xcodebuild -project Xsnmp.xcodeproj -scheme Xsnmp archive
